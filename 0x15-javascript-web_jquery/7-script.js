3f656a4cd00639de1549cf3fe94c29d2c491a108
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-02-09 08:18:22 +0000

    fix url nad markup

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-$.get('https://swapi.co/api/people/5/?format=json', function (res, status) {
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (res, status) {
   if (status === 'success') {
     $('#character').text(res.name);
   }
