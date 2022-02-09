0bfc951672e5442399675fa7d990a5140658ea60
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-02-09 08:31:15 +0000

    fix li generation

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,8 +1,8 @@
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (res, status) {
   if (status === 'success') {
-    const li = document.createElement('li');
     const films = res.results;
     for (const film of films) {
+      const li = document.createElement('li');
       li.textContent = film.title;
       $('#list_movies').append(li);
     }
